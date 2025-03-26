e353b4f8222e558aae12b47754d865d97d44f35a
aarav75620 <aarav.dontula3@gmail.com>
2025-03-26 10:54:55 +0530

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,107 @@
+#define trigPin 11
+#define echoPin 12
+
+#define in1 7
+#define in2 6
+#define in3 5
+#define in4 4
+#define enA 9
+#define enB 10
+
+// Motor speed (0-255)
+int motorSpeed = 150;
+
+// Distance threshold to detect obstacles
+int distanceThreshold = 20;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+
+  // Set motor speed
+  analogWrite(enA, motorSpeed);
+  analogWrite(enB, motorSpeed);
+
+  Serial.begin(9600);
+}
+
+// Function to measure distance
+int getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;
+
+  return distance;
+}
+
+// Move robot forward
+void moveForward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+// Move robot backward
+void moveBackward() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+// Turn robot left
+void turnLeft() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+// Turn robot right
+void turnRight() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+// Stop the robot
+void stop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void loop() {
+  int distance = getDistance();
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if (distance > distanceThreshold) {
+    moveForward();
+  } else {
+    stop();
+    delay(500);
+
+    moveBackward();
+    delay(500);
+
+    turnLeft();
+    delay(500);
+  }
+}
